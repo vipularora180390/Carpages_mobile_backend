2bacda78af36aba2bdb395c746fa9fb60748f9af
vipularora180390 <vipularora180390@gmail.com>
2023-06-29 22:46:08 +0530

    https://github.com/vipularora180390/Carpages_mobile_backend

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express =require("express");
 const bodyParser=require("body-parser");
 
 const app=express();
-const cors =require('cors');
+
 const FileUpload=require("express-fileupload");
 app.use(express.static('uploads'));
 const cookieparser=require('cookie-parser');
